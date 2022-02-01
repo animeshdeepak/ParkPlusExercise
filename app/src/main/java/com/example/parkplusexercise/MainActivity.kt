cfda0925006d55936914db119b279bb7cd36d0d3
animeshdeepak <animeshdeepak1@gmail.com>
2022-02-01 22:44:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/parkplusexercise/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.parkplusexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel : MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+    }
+}
